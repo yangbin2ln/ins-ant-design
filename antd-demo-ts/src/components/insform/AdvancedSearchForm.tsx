754863a75ca381abb4014eef6cfea65f5b5bfb19
yangbin <465471659@qq.com>
2018-08-14 14:55:49 +0800

    first

---
+++ antd-demo-ts/src/components/insform/AdvancedSearchForm.tsx
@@ -0,0 +1,90 @@
+import { Form, Row, Col, Input, Button, Icon } from 'antd';
+import * as React from 'react';
+
+const FormItem = Form.Item;
+
+class AdvancedSearchForm extends React.Component<any, any> {
+  state = {
+    expand: false,
+  };
+
+  handleSearch = (e) => {
+    e.preventDefault();
+    this.props.form.validateFields((err, values) => {
+      console.log('Received values of form: ', values, err);
+    });
+  }
+
+  handleReset = () => {
+    this.props.form.resetFields();
+  }
+
+  toggle = () => {
+    const { expand } = this.state;
+    this.setState({ expand: !expand });
+  }
+
+  getItemLayout() {
+    const formItemLayout = {
+      labelCol: {
+        xs: { span: 24 },
+        sm: { span: 8 },
+      },
+      wrapperCol: {
+        xs: { span: 24 },
+        sm: { span: 16 },
+      },
+    };
+    return formItemLayout;
+  }
+
+  // To generate mock Form.Item
+  getFields() {
+    const count = this.state.expand ? 10 : 6;
+    const { getFieldDecorator } = this.props.form;
+    const children = [];
+    for (let i = 0; i < 10; i++) {
+      children.push(
+        <Col key={i} xs={24} sm={5} style={{ display: i < count ? 'block' : 'none' }}>
+          <FormItem {...this.getItemLayout()} label={`Field ${i}`}>
+            {getFieldDecorator(`field-${i}`, {
+              rules: [{
+                required: true,
+                message: 'Input something!',
+              }],
+            })(
+              <Input placeholder="placeholder" />
+            )}
+          </FormItem>
+        </Col>
+      );
+    }
+    return children;
+  }
+
+  render() {
+    return (
+      <Form
+        className="ant-advanced-search-form"
+        onSubmit={this.handleSearch}
+      >
+        <Row gutter={8}>{this.getFields()}</Row>
+        <Row>
+          <Col span={24} style={{ textAlign: 'right' }}>
+            <Button type="primary" htmlType="submit">Search</Button>
+            <Button style={{ marginLeft: 8 }} onClick={this.handleReset}>
+              Clear
+            </Button>
+            <a style={{ marginLeft: 8, fontSize: 12 }} onClick={this.toggle}>
+              Collapse <Icon type={this.state.expand ? 'up' : 'down'} />
+            </a>
+          </Col>
+        </Row>
+      </Form>
+    );
+  }
+}
+
+const WrappedAdvancedSearchForm = Form.create()(AdvancedSearchForm);
+
+export {WrappedAdvancedSearchForm};
